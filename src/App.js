82ab7021bf78b06f0bec1966f4c84bf7118bb2e4
Tatiana Obando <PATH="/opt/homebrew/bin:$PATH>
2023-02-23 11:28:13 -0800

    Counter exercise

--- src/App.js
+++ src/App.js
@@ -3,40 +3,25 @@ import { useState } from "react";
 
 function App() {
 
-  const [age,setAge] = useState(0);
+  const [counter, setCount] = useState(0);
 
-  const [inputValue, setInputValue] = useState("");
-
-  const [showText, setShowText] = useState(true);
-
-  const [textColor, setTextColor] = useState("black");
-
-
-  const increaseAge = () => {
-    setAge(age+1);
+  const increaseCount = () => {
+    setCount(counter+1);
   }
-
-  const handleInputChange = (event) => {
-    console.log(event.target.value);
-    setInputValue(event.target.value); // value of the input that the user is writing
-  };
+  const decreaseCount = () => {
+    setCount(counter-1);
+  }
+  const setToZero = () => {
+    setCount(0);
+  }
 
 
   return ( 
     <div className="App"> 
-    {age} <button onClick={increaseAge}>Increase Age</button>
-
-    <input type="text" onChange={handleInputChange}/>
-    {inputValue}
-    <button 
-    onClick={()=> {setShowText(!showText);}}>Show or Hide
-    </button> 
-    {showText && <h3 style={{color: "red"}}>hi this is me</h3>}
-    <button 
-    onClick={()=> {
-      setTextColor(textColor=== "black" ? "yellow": "black");
-      }}>change the color</button>
-    <h4 style={{color: textColor}}> changing my color</h4>
+    <button onClick={increaseCount}>Increase</button>
+    <button onClick={decreaseCount}>Decrease</button>
+    <button onClick={setToZero}>Set to Zero</button>
+    {counter}
     </div>
 
     );
