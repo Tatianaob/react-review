a18f5d74af88f2e70cef112c828a18ee2827c91d
Tatiana Obando <PATH="/opt/homebrew/bin:$PATH>
2023-02-22 18:20:54 -0800

    Setup, jsx, components and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+    <User name="Tatiana" age={30} email="tati@tati.com"/>
+    <User name="Erika" age={39} email="erika@gmail.com"/>
+    <User name="Paola" age={26} email="paola@gmail.com"/>
+    <Job salary={9000} position="Senior SDE"company="Amazon"/>
+		<Job salary={12000} position="Junior SDE"company="Google"/>
+		<Job salary={10000} position="Project Manager"company="Netflix"/>
+  </div>;
 }
 
+
+
+const User = (props) => {
+  return ( <div>
+    <h1>{props.name}</h1>
+    <h1>{props.age}</h1>
+    <h1>{props.email}</h1>
+  </div>
+  )
+};
+
+const Job = (props) => {
+  return ( <div>
+    <h2>{props.salary}</h2>
+    <h2>{props.position}</h2>
+    <h2>{props.company}</h2>
+
+  </div>)
+}
 export default App;
