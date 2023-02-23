133b6623d8e7fb489353422a5b8c36e36f8cedf4
Tatiana Obando <PATH="/opt/homebrew/bin:$PATH>
2023-02-22 20:26:26 -0800

    created User component and added map function to loop throught the objects of each user

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 
 import './App.css';
+import {User} from './User'
 
 
 function App() {
-  const age = 28;
-  const isGreen = true;
-  // const isGreen = false;
+  const users = [
+    {name: 'Tatiana', age: 30},
+    {name: 'Bradley', age: 31},
+    {name: 'Diego', age: 1},
+  ];
 
-  return <div className="App">
-    {age >= 18 ? <h1>over age</h1> : <h1>under age</h1>}
-    <h1 style={{color: isGreen ? "green": "red"}}>this has color </h1>
-
-    {isGreen && <button>this is a button</button>}
-  </div>
+  return ( 
+    <div className="App"> 
+      {users.map((user, key) => {
+        return (
+        <User name={user.name} age={user.age}/>
+        );
+      })}
+    </div>
+  );
   }
 
 
